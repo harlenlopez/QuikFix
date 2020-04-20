faf4204571f4c5bbe65386ae12937e99007ce307
Jin Kim <jinwoov@gmail.com>
2020-04-20 15:08:57 -0700

    DbContextAdded to the UserApplication

--- e-CommerceMVC/e-CommerceMVC/Program.cs
+++ e-CommerceMVC/e-CommerceMVC/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace e_CommerceMVC
+namespace ECommerceMVC
 {
     public class Program
     {
