7806c840ee1ab813f0c18c68f1e035122f38461a
harlen lopez <harlen.d.lopez@gmail.com>
2020-04-21 13:58:36 -0700

    CRUD operations for our inventory.

---
+++ e-CommerceMVC/e-CommerceMVC/Models/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using ECommerceMVC.Models.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceMVC.Models
+{
+    public class Inventory
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
