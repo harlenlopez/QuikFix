b0d5e792efb5658335d5f274bf8c835cd8d81cf4
harlen lopez <harlen.d.lopez@gmail.com>
2020-04-21 15:07:42 -0700

    updated DbContext

---
+++ e-CommerceMVC/e-CommerceMVC/Data/StoreDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ECommerceMVC.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceMVC.Data
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Inventory> Inventories { get; set; }
+
+    }
+}
---
+++ e-CommerceMVC/e-CommerceMVC/Models/Service/InventoryService.cs
@@ -0,0 +1,45 @@
+﻿using ECommerceMVC.Data;
+using ECommerceMVC.Models.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+
+namespace ECommerceMVC.Models.Service
+{
+    public class InventoryService : IInventoryManager
+    {
+        private readonly StoreDbContext _context;
+
+        public InventoryService(StoreDbContext context)
+        {
+            _context = context;
+
+        }
+        public async Task<Inventory> CreateInventory(Inventory inventory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteInventories(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Inventory>> GetAllInventories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Inventory>> GetInventoryById(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateInventories(Inventory inventory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
