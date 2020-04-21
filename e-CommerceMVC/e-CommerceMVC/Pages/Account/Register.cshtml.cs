634ad934c67dd65c5e36abcd1baadd557668273e
Jin Kim <jinwoov@gmail.com>
2020-04-21 14:40:13 -0700

    added our logic to post method

--- e-CommerceMVC/e-CommerceMVC/Pages/Account/Register.cshtml.cs
+++ e-CommerceMVC/e-CommerceMVC/Pages/Account/Register.cshtml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using ECommerceMVC.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -10,9 +12,50 @@ namespace ECommerceMVC.Pages.Account
 {
     public class RegisterModel : PageModel
     {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        [BindProperty]
         public RegisterInfo RegisterData { get; set; }
+
+        public RegisterModel(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    UserName = RegisterData.Email,
+                    Email = RegisterData.Email,
+                    FirstName = RegisterData.FirstName,
+                    LastName = RegisterData.LastName,
+                    BirthDate = RegisterData.BirthDay
+                };
+
+                var result = await _userManager.CreateAsync(user, RegisterData.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(String.Empty, error.Description);
+                }
+            }
+
+            return Page();
         }
 
         public class RegisterInfo
@@ -33,7 +76,7 @@ namespace ECommerceMVC.Pages.Account
             [Required]
             [DataType(DataType.Date)]
             [Display(Name = "Birth day")]
-            public string BirthDay { get; set; }
+            public DateTime BirthDay { get; set; }
 
             [Required]
             [StringLength(10, ErrorMessage = "The {0} must be at least {2} and max of {1} characters long", MinimumLength = 8)]
