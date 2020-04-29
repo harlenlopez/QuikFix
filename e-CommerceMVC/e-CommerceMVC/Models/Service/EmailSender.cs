a939c479cd10eab531f0b558af15851fbac51f9e
Jin Kim <jinwoov@gmail.com>
2020-04-29 13:00:34 -0700

    IEmailSender working

---
+++ e-CommerceMVC/e-CommerceMVC/Models/Service/EmailSender.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceMVC.Models.Service
+{
+    /// <summary>
+    /// Implementing the IEmail sender to the method and having to bringing the built in methods from Microsoft library
+    /// </summary>
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
