bf87cdda4bd7a3491c5952eed1dc2bac0768e7cb
Jin Kim <jinwoov@gmail.com>
2020-05-04 15:31:31 -0700

    Everything is working

---
+++ e-CommerceMVC/e-CommerceMVC/Models/Interface/IPayment.cs
@@ -0,0 +1,13 @@
+﻿using AuthorizeNet.Api.Contracts.V1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceMVC.Models.Interface
+{
+    public interface IPayment
+    {
+        bool Run(customerAddressType addressType, decimal totalAmount);
+    }
+}
