8f425c48808178fe300328a02940bb42569153b8
harlen lopez <harlen.d.lopez@gmail.com>
2020-05-04 13:50:33 -0700

    milestone one,

---
+++ e-CommerceMVC/e-CommerceMVC/Models/Service/PaymentService.cs
@@ -0,0 +1,25 @@
+﻿using AuthorizeNet.Api.Contracts.V1;
+using AuthorizeNet.Api.Controllers.Bases;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceMVC.Models.Service
+{
+    public class PaymentService
+    {
+        private readonly IConfiguration _config;
+
+        public PaymentService(IConfiguration configuration)
+        {
+            _config = configuration;
+
+        }
+        public string Run()
+        {
+            ApiOperationBase<ANetApiRequest, ANetApiResponse>.RunEnvironment = AuthorizeNet.Environment.SANDBOX;
+        }
+    }
+}
