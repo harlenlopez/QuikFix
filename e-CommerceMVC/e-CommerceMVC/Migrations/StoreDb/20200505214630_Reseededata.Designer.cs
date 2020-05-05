b3f61333ca62276c696666a52ceacf57b978f628
Jin Kim <jinwoov@gmail.com>
2020-05-05 14:49:51 -0700

    updated seed data with azure blob images

--- e-CommerceMVC/e-CommerceMVC/Migrations/StoreDb/20200505214630_Reseededata.Designer.cs
+++ e-CommerceMVC/e-CommerceMVC/Migrations/StoreDb/20200505214630_Reseededata.Designer.cs
@@ -3,14 +3,16 @@ using ECommerceMVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ECommerceMVC.Migrations.StoreDb
 {
     [DbContext(typeof(StoreDbContext))]
-    partial class StoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200505214630_Reseededata")]
+    partial class Reseededata
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
