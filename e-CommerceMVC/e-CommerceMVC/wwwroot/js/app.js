c469609bdcc87b2bcab08fde82828a095ad54a9d
Jin Kim <jinwoov@gmail.com>
2020-05-16 18:26:58 -0700

    Finished everything so far

--- e-CommerceMVC/e-CommerceMVC/wwwroot/js/app.js
+++ e-CommerceMVC/e-CommerceMVC/wwwroot/js/app.js
@@ -1,1 +1,13 @@
-﻿
+﻿$('#Cart-button').click(clickCart)
+
+function clickCart() {
+    let count = 1;
+    $('.sidebar').toggle();
+    count++;
+    $('#main-body-box').toggleClass();
+
+}
+
+$('.sidebar').hide();
+$('#main-body-box').toggleClass();
+
